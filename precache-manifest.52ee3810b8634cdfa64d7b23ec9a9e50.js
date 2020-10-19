d8a231508ef07d0b1007c5c8020cf15d51e3fe7f
Jivin Varghese <jivinvarghese007@gmail.com>
2020-10-19 12:31:19 +0530

    Updates

---
+++ precache-manifest.52ee3810b8634cdfa64d7b23ec9a9e50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b8ede8e9951dd58f38759642c8086b6",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "c411d72d8981adaeb1c4",
+    "url": "/todoapp/static/css/main.fafdff6b.chunk.css"
+  },
+  {
+    "revision": "01c70015f4eb54055909",
+    "url": "/todoapp/static/js/2.f0f043cd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todoapp/static/js/2.f0f043cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c411d72d8981adaeb1c4",
+    "url": "/todoapp/static/js/main.30efea64.chunk.js"
+  },
+  {
+    "revision": "80b05f8f46ecf0bf2c2b",
+    "url": "/todoapp/static/js/runtime-main.5545d9fc.js"
+  }
+]);
